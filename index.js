3beb4c84bdb2dc3a19cfc48bfd747cd12aed9196
cheunm01 <manyee.cheung@bbc.co.uk>
2025-01-28 18:23:21 +0000

    logic for plant limit, no plant selection, less than 4 plants selected

--- index.js
+++ index.js
@@ -1,33 +1,67 @@
-// each question is checkable | JS - id for each flower/ plant - document.getElementById('id')--->>> unquie id/name for each flower/plant
-// when the user clicks the button then it is submitted to the server
+// Logic:
 // happy path: user selects 4 flower and clicks submit
 // sad path 1: user does not select a flower and clicks submit - error message
 // sad path 2: user onlys selects one or less than for flower and clicks submit - error message
 // sad path 3: user selects more than 4 flowers and clicks submit - error message
 
 
-// Task List
-// create list of json data of the plants
-// create onsumbit button -> console.log('submit button clicked')
-
-const daisy = document.getElementById('daisy');
 const formSubmitButton = document.getElementById('submit');
 const formElements = document.querySelectorAll('input[type="checkbox"]');
 
-formSubmitButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    formSubmit();
-});
+let data = [];
+const PLANT_LIMIT = 4;
 
 
+function resetData(data) {
+  return (data.length = 0);
+}
+
+
+let checkLimit = () => {
+    if (data.length === 0) {
+      alert('Please make a selection');
+      resetData(data);
+      return false;
+    }
+
+    if (data.length > PLANT_LIMIT) {
+      alert('You can only select 4 plants');
+      resetData(data);
+      return false;
+    }
+
+    if (data.length > 0 && data.length < PLANT_LIMIT) {
+      alert('Please select your 4 favourite plants');
+      return false;
+    }
+};
+
+function checkedFormElements() {
+    formElements.forEach((element) => {
+        if (element.checked === true) {
+          return data.push(element.value);
+        }
+    });
+}
+
+
+function getPollResults() {
+    checkedFormElements();
+    checkLimit();
+}
 
 function formSubmit() {
-  let data = [];
-  formElements.forEach((flower) => {
-      if(flower.checked === true) {
-          console.log(flower);
-          data.push(flower.value);
-      }
-  });
+  if (data.length >= 0 && data) {
+    resetData(data);
+  }
+  getPollResults();
+  console.log(data);
   return data;
 }
+
+(function start() {
+  formSubmitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    formSubmit();
+  });
+})();
